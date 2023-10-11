2a26827a912dcd3801e9acefbe833bab3e18eb4f
Muhammad Rahmatullah <rahmatthea5@gmail.com>
2023-10-11 19:59:29 +0700

    init project

---
+++ lib/vending_machine_perqara.ex
@@ -0,0 +1,18 @@
+defmodule VendingMachinePerqara do
+  @moduledoc """
+  Documentation for `VendingMachinePerqara`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> VendingMachinePerqara.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/vending_machine_perqara_test.exs
@@ -0,0 +1,8 @@
+defmodule VendingMachinePerqaraTest do
+  use ExUnit.Case
+  doctest VendingMachinePerqara
+
+  test "greets the world" do
+    assert VendingMachinePerqara.hello() == :world
+  end
+end
