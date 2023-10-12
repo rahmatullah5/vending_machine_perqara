73f739d384b7572863b11eab0417184a9e42bf4c
Muhammad Rahmatullah <rahmatthea5@gmail.com>
2023-10-12 11:10:39 +0700

    Create solution with pattern maching and macro approach

--- lib/vending_machine_perqara.ex
+++ lib/vending_machine_perqara.ex
@@ -3,16 +3,38 @@ defmodule VendingMachinePerqara do
   Documentation for `VendingMachinePerqara`.
   """
 
-  @doc """
-  Hello world.
+  @products [
+    {12000, "Coffee"},
+    {9000, "Milo"},
+    {7000, "Cola"},
+    {5000, "Sosro"},
+    {2000, "Aqua"}
+  ]
 
-  ## Examples
+  @valid_denominations [2000, 5000]
 
-      iex> VendingMachinePerqara.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  @spec run(maybe_improper_list()) :: {:error, <<_::64, _::_*8>>} | map()
+  def run(input) do
+    with {:ok, sum_of_input} <- filter_and_sum(input) do
+      buy_products(sum_of_input, %{})
+    end
   end
+
+  defp filter_and_sum([], acc \\ 0), do: {:ok, acc}
+
+  defp filter_and_sum([head | _tail], _acc) when head not in @valid_denominations,
+    do: {:error, "#{head} is invalid denomination"}
+
+  defp filter_and_sum([head | tail], acc), do: filter_and_sum(tail, acc + head)
+
+  for {price, name} <- @products do
+    defp buy_products(balance, purchased_products) when balance >= unquote(price) do
+      buy_products(
+        balance - unquote(price),
+        Map.update(purchased_products, unquote(name), 1, fn n -> n + 1 end)
+      )
+    end
+  end
+
+  defp buy_products(_balance, purchased_products), do: purchased_products
 end
