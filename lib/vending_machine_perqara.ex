a8665a44f17978b4fdb6233d5cedb2931b3febf3
Muhammad Rahmatullah <rahmatthea5@gmail.com>
2023-10-13 16:42:57 +0700

    Create test and add Slicing Window alternatives

--- lib/vending_machine_perqara.ex
+++ lib/vending_machine_perqara.ex
@@ -38,3 +38,52 @@ defmodule VendingMachinePerqara do
 
   defp buy_products(_balance, purchased_products), do: purchased_products
 end
+
+defmodule VendingMachinePerqaraSlidingWindow do
+  @products %{
+    12000 => "Coffee",
+    9000 => "Milo",
+    7000 => "Cola",
+    5000 => "Sosro",
+    2000 => "Aqua"
+  }
+
+  @valid_denominations [2000, 5000]
+
+  def run(input, products \\ @products, valid_denominations \\ @valid_denominations) do
+    with {:ok, sum_of_input} <- filter_and_sum(input, valid_denominations) do
+      sorted_product_prices = Map.keys(products) |> Enum.sort(fn a, b -> a > b end)
+
+      buy_products(sum_of_input, sorted_product_prices, products, %{})
+    end
+  end
+
+  defp filter_and_sum([], _valid_denominations, acc \\ 0), do: {:ok, acc}
+
+  defp filter_and_sum([head | tail], valid_denominations, acc) do
+    if head in valid_denominations do
+      filter_and_sum(tail, valid_denominations, acc + head)
+    else
+      {:error, "#{head} is invalid denomination"}
+    end
+  end
+
+  defp buy_products(balance, [], _products, purchased_products), do: purchased_products
+
+  defp buy_products(
+         balance,
+         [product_price | rest_of_prices] = sorted_product_prices,
+         products,
+         purchased_products
+       ) do
+    if balance >= product_price do
+      product_name = Map.get(products, product_price)
+
+      purchased_products = Map.update(purchased_products, product_name, 1, fn n -> n + 1 end)
+
+      buy_products(balance - product_price, sorted_product_prices, products, purchased_products)
+    else
+      buy_products(balance, rest_of_prices, products, purchased_products)
+    end
+  end
+end
--- test/vending_machine_perqara_test.exs
+++ test/vending_machine_perqara_test.exs
@@ -2,7 +2,88 @@ defmodule VendingMachinePerqaraTest do
   use ExUnit.Case
   doctest VendingMachinePerqara
 
-  test "greets the world" do
-    assert VendingMachinePerqara.hello() == :world
+  describe "VendingMachinePerqara" do
+    test "returns an error when an invalid denomination is input" do
+      assert {:error, _} = VendingMachinePerqara.run([3000])
+    end
+
+    test "returns a map of products that can be purchased with the valid denominations provided" do
+      assert %{"Aqua" => 1} = VendingMachinePerqara.run([2000])
+
+      assert %{"Aqua" => 2} = VendingMachinePerqara.run([2000, 2000])
+
+      assert %{"Cola" => 1} = VendingMachinePerqara.run([5000, 2000])
+
+      assert %{"Milo" => 1} = VendingMachinePerqara.run([5000, 5000])
+
+      assert %{"Coffee" => 1, "Sosro" => 1} = VendingMachinePerqara.run([5000, 5000, 5000, 2000])
+
+      assert %{"Coffee" => 1} = VendingMachinePerqara.run([2000, 5000, 5000])
+      assert %{"Coffee" => 1, "Aqua" => 1} = VendingMachinePerqara.run([2000, 5000, 2000, 5000])
+    end
+  end
+end
+
+defmodule VendingMachinePerqaraSlidingWindowTest do
+  use ExUnit.Case
+
+  describe "VendingMachinePerqaraSlidingWindow" do
+    test "returns an error when an invalid denomination is input" do
+      assert {:error, _} = VendingMachinePerqaraSlidingWindow.run([1000])
+    end
+
+    test "returns a map of products that can be purchased with the valid denominations provided" do
+      assert %{"Aqua" => 1} = VendingMachinePerqaraSlidingWindow.run([2000])
+
+      assert %{"Aqua" => 2} = VendingMachinePerqaraSlidingWindow.run([2000, 2000])
+
+      assert %{"Cola" => 1} = VendingMachinePerqaraSlidingWindow.run([5000, 2000])
+
+      assert %{"Milo" => 1} = VendingMachinePerqaraSlidingWindow.run([5000, 5000])
+
+      assert %{"Coffee" => 1, "Sosro" => 1} =
+               VendingMachinePerqaraSlidingWindow.run([5000, 5000, 5000, 2000])
+
+      assert %{"Coffee" => 1} = VendingMachinePerqaraSlidingWindow.run([5000, 5000, 2000])
+
+      assert %{"Coffee" => 1, "Aqua" => 1} =
+               VendingMachinePerqaraSlidingWindow.run([5000, 2000, 5000, 2000])
+    end
+
+    test "returns an empty map when not enough money is provided for any product" do
+      custom_products = %{
+        3000 => "Custom Drink",
+        4000 => "Custom Snack"
+      }
+
+      assert %{} =
+               VendingMachinePerqaraSlidingWindow.run(
+                 [2000],
+                 custom_products
+               )
+    end
+
+    test "returns the correct products even when custom products and denominations are used" do
+      custom_products = %{
+        3000 => "Custom Drink",
+        4000 => "Custom Snack"
+      }
+
+      custom_denominations = [1000, 2000]
+
+      assert %{"Custom Drink" => 1} =
+               VendingMachinePerqaraSlidingWindow.run(
+                 [1000, 2000],
+                 custom_products,
+                 custom_denominations
+               )
+
+      assert %{"Custom Snack" => 1, "Custom Drink" => 1} =
+               VendingMachinePerqaraSlidingWindow.run(
+                 [2000, 2000, 1000, 2000],
+                 custom_products,
+                 custom_denominations
+               )
+    end
   end
 end
